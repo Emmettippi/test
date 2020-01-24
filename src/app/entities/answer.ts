c1e4c47150c3cca29ac71793f6132a7797416a50
Yuri <yuri.poggi@objectmethod.it>
2020-01-24 09:58:05 +0100

    Added ponts to every answer

--- src/app/entities/answer.ts
+++ src/app/entities/answer.ts
@@ -1,9 +1,11 @@
 export class Answer {
     id: number;
     answers: number[];
+    values: number[];
 
     constructor(id?: number) {
         this.id = id || null;
         this.answers = [];
+        this.values = [];
     }
 }
